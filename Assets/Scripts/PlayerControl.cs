c167259b990f59a0df8a335d2c0b6d23bd4f64fd
Huihan <hh2720@nyu.edu>
2023-03-05 01:51:36 -0500

    little step for me huge step for human; available to activate but not yet destroy prefab

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms.Impl;
@@ -9,6 +10,7 @@ public class PlayerControl : MonoBehaviour
 
     public float speed = 2f;
     public float timer = 20;
+    public float distanceLimit = 2f;
 
     Rigidbody2D myBody;
     Animator myAnim;
@@ -25,6 +27,21 @@ public class PlayerControl : MonoBehaviour
     public AudioClip speech;
 
     bool GetKey = false;
+
+    //Trial
+
+    public GameObject npcOutside;
+    public GameObject npcInside;
+
+    float distanceX;
+    float distanceY;
+    float distanceZ;
+
+    float DistanceX;
+    float DistanceY;
+    float DistanceZ;
+
+    public GameObject speechBubble;
 
     // Start is called before the first frame update
     void Start()
@@ -57,6 +74,66 @@ public class PlayerControl : MonoBehaviour
 
         transform.position = newPos;
 
+        //Trial
+
+        Vector3 delta = transform.position - npcOutside.transform.position;
+        distanceX = delta.x;
+        distanceY = delta.y;
+        distanceZ = delta.z;
+
+        Debug.Log(DistanceY);
+
+        if ((-distanceLimit <= distanceX && distanceX <= distanceLimit) && (-distanceLimit-0.2 <= distanceY && distanceY <= distanceLimit))
+        {
+            Debug.Log("Detected");
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Ready Sir");
+
+                Vector3 bubblePos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+                mySource.PlayOneShot(speech);
+
+                if (GetKey)
+                {
+                    //GameObject bubbleForRemove = Instantiate(speechBubble, bubblePos, Quaternion.identity);
+                    Speech2.SetActive(true);
+                }
+                else
+                {
+                    //Instantiate(speechBubble, bubblePos, Quaternion.identity);
+                    Speech1.SetActive(true);
+                }
+            }
+        }
+        else
+        {
+            Speech1.SetActive(false);
+            Speech2.SetActive(false);
+            //Destroy(bubbleForRemove);
+        }
+
+        //Second
+
+        Vector3 alpha = transform.position - npcInside.transform.position;
+        DistanceX = alpha.x;
+        DistanceY = alpha.y;
+
+        if ((-distanceLimit <= DistanceX && DistanceX <= distanceLimit) && (-distanceLimit - 0.2 <= DistanceY && DistanceY <= distanceLimit))
+        {
+            Debug.Log("Detected Again");
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                mySource.PlayOneShot(speech);
+                Speech3.SetActive(true);
+            }
+        }
+        else
+        {
+            Speech3.SetActive(false);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D somecollision)
@@ -65,8 +142,12 @@ public class PlayerControl : MonoBehaviour
 
         if (somecollision.gameObject.name == "obj_npcOutside")
         {
+
             if (Input.GetKey(KeyCode.Space))
             {
+
+                Debug.Log("Ready Sir");
+
                 mySource.PlayOneShot(speech);
 
                 if (GetKey)
